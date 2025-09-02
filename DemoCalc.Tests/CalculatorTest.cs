d7bcef96d0f07582221b98a63b9aca05f4a011b5
Stanton Roux <stanton@conceptlab.co.za>
2025-09-02 20:40:52 +0200

    commit all

---
+++ DemoCalc.Tests/CalculatorTest.cs
@@ -0,0 +1,7 @@
+﻿using DemoCalc;
+using Xunit;
+
+public class CalculatorTests
+{
+    [Fact] public void Add_Works() => Assert.Equal(5, Calculator.Add(2, 3));
+}
---
+++ DemoCalc/Calculator.cs
@@ -0,0 +1,5 @@
+﻿namespace DemoCalc;
+public static class Calculator
+{
+    public static int Add(int a, int b) => a + b;
+}
